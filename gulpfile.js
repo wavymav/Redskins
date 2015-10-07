530bac292b94b4d945713e97e6d7430ee31c441c
Marcus Barnes <mavjbarnes@gmail.com>
2015-10-07 15:31:48 -0400

    set up the default Gulp task to convert the JSX in /src into JS and store it app.js in the root directory

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp'); // Requiring the Gulp module
+var react = require('gulp-react'); // Requiring the Gulp React module
+var concat = require('gulp-concat'); // Requiring the Gulp Concat module
+
+// The main task (default task)
+gulp.task('default', function() {
+	// Grabbing all the files in src folder
+	return gulp.src('src/**')
+		// Connvert the JSX in /src into JS
+		.pipe(react())
+		// When finished then concat the js into app.js
+		.pipe(concat('app,js'))
+		// Then save the app.js file in the root directory
+		.pipe(gulp.dest('./'));
+});
